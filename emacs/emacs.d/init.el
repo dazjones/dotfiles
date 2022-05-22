3aaacd7db49012ef6c53d5f7cee1b1f91f5f582a
Darren Jones <darren@djns.io>
2022-05-22 22:51:50 +0100

    Removed unwanted custom-set-faces

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -380,9 +380,3 @@
 (use-package counsel-projectile
   :after projectile
   :config (counsel-projectile-mode))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
