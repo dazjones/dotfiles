7ac57493b6e658c5a43ae6dc42c6d6e1dde6f5c3
Darren Jones <darren@darrenjonesdesign.co.uk>
2022-06-17 17:04:48 +0100

    simplify vim

--- install.sh
+++ install.sh
@@ -28,7 +28,6 @@ link_dotfile() {
   fi
 }
 
-mkdir ~/.emacs.d/
 
 link_dotfile "zsh/zshrc" ".zshrc"
 link_dotfile "zsh/aliases" ".aliases"
@@ -36,4 +35,3 @@ link_dotfile "tmux/tmux" ".tmux"
 link_dotfile "tmux/tmux.conf" ".tmux.conf"
 link_dotfile "vim/vim" ".vim"
 link_dotfile "vim/vimrc" ".vimrc"
-link_dotfile "emacs/.emacs.d/init.el" ".emacs.d/init.el"
